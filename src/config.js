77acdbb193998f2c63091ae5360648252d8f9556
John Bowser <john.bow7720@gmail.com>
2020-10-05 13:57:59 -0600

    added heroku endpoint to config

--- src/config.js
+++ src/config.js
@@ -1,4 +1,6 @@
 export default {
-  API_ENDPOINT: 'http://localhost:8000/api',
+  API_ENDPOINT: 'https://murmuring-shelf-10969.herokuapp.com/api',
   TOKEN_KEY: 'blogful-client-auth-token',
 }
+
+// 'http://localhost:8000/api' <= dev server address
