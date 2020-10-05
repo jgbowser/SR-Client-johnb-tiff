329c8e920285dd695f455cfca534b3347fd7c4c9
Tiff Joy <tiffanysummerford@gmail.com>
2020-10-05 15:46:52 -0400

    html temp done

--- src/routes/DashboardRoute/DashboardRoute.js
+++ src/routes/DashboardRoute/DashboardRoute.js
@@ -4,7 +4,33 @@ class DashboardRoute extends Component {
   render() {
     return (
       <section>
-        implement and style me
+        <h2>Spanish</h2>
+        <h3>Start Practicing</h3>
+        <button>Start</button>
+        <h4>Words to practice...</h4>
+          <ul>
+            <li>bruja</li>
+            <li>bruja</li>
+            <li>bruja</li>
+            <li>bruja</li>
+            <li>bruja</li>
+            <li>bruja</li>
+            <li>bruja</li>
+            <li>bruja</li>
+          </ul>
+          <h4>Word || Correct Answer Count || Incorrect Answer Count</h4>
+            <ul>
+            <li>bruja || 1 || 0</li>
+            <li>bruja || 1 || 0</li>
+            <li>bruja || 1 || 0</li>
+            <li>bruja || 1 || 0</li>
+            <li>bruja || 1 || 0</li>
+            <li>bruja || 1 || 0</li>
+            <li>bruja || 1 || 0</li>
+            <li>bruja || 1 || 0</li>
+            </ul>
+          <h4>Total Correct</h4>
+            <p>8</p>
       </section>
     );
   }
