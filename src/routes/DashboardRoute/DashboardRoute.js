125b7c9ae12e445489dc68cc2a0c648f3e8514df
Tiff Joy <tiffanysummerford@gmail.com>
2020-10-06 16:16:03 -0400

    css

--- src/routes/DashboardRoute/DashboardRoute.js
+++ src/routes/DashboardRoute/DashboardRoute.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react'
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import LanguageApiService from '../../services/language-service'
 import LanguageContext from '../../contexts/LanguageContext'
 import WordListItem from '../../components/WordListItem/WordListItem'
+import dashStyles from './DashboardRoute.module.css'
 
 class DashboardRoute extends Component {
 
@@ -18,19 +19,19 @@ class DashboardRoute extends Component {
   }
 
   render() {
-    
+
     const { language, words } = this.context
-    const wordList = words.map(word =>  <WordListItem word={word}/> )
+    const wordList = words.map(word => <WordListItem word={word} />)
     return (
-      <section>
+      <section className={dashStyles.dashboard}>
         <h2>{language.name}</h2>
-        <Link to='/learn'>Start practicing</Link>
+        <Link to='/learn' className={dashStyles.link}>Start practicing</Link>
         <h3>Words to practice</h3>
-        
+
         <ul>
-         {wordList}
+          {wordList}
         </ul>
-      
+
         <p>Total correct answers: {language.total_score}</p>
       </section>
     );
