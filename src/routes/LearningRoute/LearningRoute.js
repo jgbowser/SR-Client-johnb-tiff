72ad916d2eb32fa7571868c3b9b041bce3a1fe96
Tiff Joy <tiffanysummerford@gmail.com>
2020-10-07 11:21:08 -0400

    wireframe

--- src/routes/LearningRoute/LearningRoute.js
+++ src/routes/LearningRoute/LearningRoute.js
@@ -4,7 +4,16 @@ class LearningRoute extends Component {
   render() {
     return (
       <section>
-        implement and style me
+        <h2>Translate the word:</h2>
+        <span>bruja</span>
+        <p>Your total score is: </p>
+        <form>
+          <label htmlFor='learn-guess-input'>What's the translation for this word?</label>
+            <input type='text' htmlFor='learn-guess-input' required></input>
+            <button type='submit'>Submit</button>
+        </form>
+        <p>You have answered this word correctly 0 times.</p>
+        <p>You have answered this word incorrectly 0 times.</p>
       </section>
     );
   }
