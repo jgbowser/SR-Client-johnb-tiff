78d4a2af2e26daff924dfb553f8b9fdefe84f3b1
Tiff Joy <tiffanysummerford@gmail.com>
2020-10-07 11:54:52 -0400

    working

--- src/routes/LearningRoute/LearningRoute.js
+++ src/routes/LearningRoute/LearningRoute.js
@@ -1,19 +1,49 @@
 import React, { Component } from 'react'
+import LanguageApiService from '../../services/language-service'
 
 class LearningRoute extends Component {
+  state = {
+    nextWord: '',
+    wordCorrectCount: '',
+    wordIncorrectCount: '',
+    totalScore: '',
+    hasError: null
+  }
+
+  setStartWord = (data) => {
+    this.setState({ 
+      nextWord: data.nextWord, 
+      wordCorrectCount: data.wordCorrectCount, 
+      wordIncorrectCount: data.wordIncorrectCount, 
+      totalScore: data.totalScore
+    })
+  }
+
+  async componentDidMount() {
+    try { let wordData = await LanguageApiService.getStartWord()
+      console.log(wordData)
+      this.setStartWord(wordData)
+    }
+    catch (error) {
+      this.setState({hasError: error})
+    }
+  }
+
   render() {
+
+    const { nextWord, wordCorrectCount, wordIncorrectCount, totalScore } = this.state
     return (
       <section>
         <h2>Translate the word:</h2>
-        <span>bruja</span>
-        <p>Your total score is: </p>
+        <span>{nextWord}</span>
+        <p>Your total score is: {totalScore}</p>
         <form>
           <label htmlFor='learn-guess-input'>What's the translation for this word?</label>
             <input type='text' htmlFor='learn-guess-input' required></input>
             <button type='submit'>Submit</button>
         </form>
-        <p>You have answered this word correctly 0 times.</p>
-        <p>You have answered this word incorrectly 0 times.</p>
+        <p>You have answered this word correctly {wordCorrectCount} times.</p>
+        <p>You have answered this word incorrectly {wordIncorrectCount} times.</p>
       </section>
     );
   }
--- src/services/language-service.js
+++ src/services/language-service.js
@@ -14,14 +14,14 @@ const LanguageApiService = {
             : res.json()
       })
   },
-  getWords() {
+  getStartWord() {
     return fetch(`${config.API_ENDPOINT}/language/head`, {
         headers: {
             'authorization' : `bearer ${TokenService.getAuthToken()}`,
           },
     })
       .then((res) => {
-          (!res.ok)
+          return (!res.ok)
             ? res.json().then(e => Promise.reject(e))
             : res.json()
       })
